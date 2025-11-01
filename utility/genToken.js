06eab03919814112e4857822542e3f40f0aa9f9d
dullat <142017979+Dullat@users.noreply.github.com>
2025-11-01 18:34:01 +0530

    genToken to gen ACCESS and REFRESH Tokens using JWT

---
+++ utility/genToken.js
@@ -0,0 +1,13 @@
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const genAccessToken = (user) => {
+  return jwt.sign(user, process.env.JWT_ACCESS_SECRET, { expiresIn: "5m" });
+};
+
+const genRefreshToken = (user) => {
+  return jwt.sign(user, process.env.JWT_ACCESS_SECRET, { expiresIn: "7d" });
+};
+
+module.exports = { genAccessToken, genRefreshToken };
