55abc1529f1249c09a870072812b2344a7244e0f
dullat <142017979+Dullat@users.noreply.github.com>
2025-11-01 18:36:33 +0530

    update app.js

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const errorHandler = require("./middleware/errorhandler.middleware");
+const notFoundError = require("./errors/NotFound.error");
+
+const authRouter = require("./routes/auth.route");
+
+const app = express();
+
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+app.use(express.json());
+
+app.use("/", authRouter);
+
+app.get("/status", (req, res) => {
+  res.json({
+    service: "Ev-project",
+    status: "OK",
+    uptime: process.uptime(),
+    timestamp: new Date().toISOString(),
+  });
+});
+
+app.use((req, res, next) => {
+  throw new notFoundError(`Route '${req.originalUrl}' does not exist`);
+});
+
+app.use(errorHandler);
+
+module.exports = app;
