aab885ad930be0e060350562dc129ececc37b290
dullat <142017979+Dullat@users.noreply.github.com>
2025-11-01 18:29:37 +0530

    make userToken model to store refresh tokens on db

---
+++ models/userToken.model.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const userTokenSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Types.ObjectId,
+    ref: "user",
+    required: [true, "Pleasse provide user id"],
+  },
+  refreshToken: {
+    type: String,
+    required: [true, "Pleasse provide token"],
+  },
+  userAgent: {
+    type: String,
+    required: [true, "Pleasse provide user agent"],
+  },
+  ip: {
+    type: String,
+    required: [true, "Pleasse provide ip"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  lastUsed: {
+    type: Date,
+  },
+  expiresAt: {
+    type: Date,
+    required: true,
+    index: { expires: 0 },
+  },
+});
+
+module.exports = mongoose.model("Usertoken", userTokenSchema);
